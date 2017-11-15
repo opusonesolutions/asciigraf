a05778c62bea3fb827dd87c2fcbb9c299ce442cc
Sam Bull <sam@pocketuniverse.ca>
2017-11-15 11:49:33 -0500

    Explicitly attribute project to Opus One Solutions

--- asciigraf/__init__.py
+++ asciigraf/__init__.py
@@ -1,1 +1,8 @@
+#############################################################################
+# Copyright (c) 2017-present, Opus One Energy Solutions Corporation
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#############################################################################
+
 from .asciigraf import graph_from_ascii
---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+#############################################################################
+# Copyright (c) 2017-present, Opus One Energy Solutions Corporation
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#############################################################################
