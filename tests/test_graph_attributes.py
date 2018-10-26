c06d9d52bf41f0e1f9f9e8c22148eafc4bfa8745
Anton Lodder <alodder@opusonesolutions.com>
2018-10-26 12:35:58 -0400

    Add copyright statements

--- tests/test_graph_attributes.py
+++ tests/test_graph_attributes.py
@@ -1,3 +1,10 @@
+#############################################################################
+# Copyright (c) 2017-present, Opus One Energy Solutions Corporation
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#############################################################################
+
 import networkx
 
 from asciigraf import graph_from_ascii
